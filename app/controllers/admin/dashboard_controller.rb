170a08df23cbfcb46defd300e8d56d71fa3fe9d7
Ibirn <kmcricciuti@gmail.com>
2020-10-14 23:56:06 +0000

    Add product/category counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   # raise ENV.inspect
   http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PW']
   def show
+    @products = Product.count
+    @categories = Category.count
   end
 end
